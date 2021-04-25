db7ed6dc82febaeebb61417a25d4dd92802dcd30
uranium93 <19hanafi93@gmail.com>
2021-04-25 16:19:25 +0200

    feat: add some seed data

---
+++ seeders/20210425103857-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('categories', [
+      { parent_id: 0, name: 'test' },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
