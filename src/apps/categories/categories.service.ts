8ee1b0e47428352ff22481685b0fad654877db74
uranium93 <19hanafi93@gmail.com>
2021-04-25 11:54:23 +0200

    chore: define Tree types

---
+++ src/apps/categories/categories.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { QueryTypes } from 'sequelize';
+import { Categories } from 'src/models';
+import {
+  getFullCategoriesTreeQuery,
+  getCategoryTreeByRootIdQuery,
+} from './queries';
+
+type CategoriesTreeQueryByRootIdResult = Categories & {
+  node_level: number;
+};
+
+type FullCategoriesTreeQueryResult = Categories & {
+  node_level: number;
+  origin: number;
+};
+
+type CategoriesTree = {
+  id: string;
+  name: string;
+  parent_id: number;
+  nodes: CategoriesTree[] | null;
+};
+
+type TreeLevels = {
+  [id: string]: {
+    id: string;
+    name: string;
+    parent_id: number;
+    nodes: CategoriesTree[] | null;
+  };
+}[];
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectModel(Categories)
+    private categoriesModel: typeof Categories,
+  ) {}
+
+}
